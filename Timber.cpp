10c81b1de1fac887d24f7637d0caeeab0e26967f
Aaron <aaron@thelinkfamily.net>
2025-01-11 01:31:01 -0600

    Setup SFML for project

--- Timber.cpp
+++ Timber.cpp
@@ -1,6 +1,13 @@
 // Timber.cpp : This file contains the 'main' function. Program execution begins and ends there.
 
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
 int main()
 {
+    //Create and open a window for the game
+    RenderWindow window(VideoMode(1920, 1080), "Timber", Style::Fullscreen);
+
     return 0;
 }

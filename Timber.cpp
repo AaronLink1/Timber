96eacc15c22483946ba2919141db3cf8b17b8d02
Aaron <aaron@thelinkfamily.net>
2025-01-11 17:05:50 -0600

    Established the game loop

--- Timber.cpp
+++ Timber.cpp
@@ -9,5 +9,20 @@ int main()
     //Create and open a window for the game
     RenderWindow window(VideoMode(1920, 1080), "Timber", Style::Fullscreen);
 
+    //GameLoop
+    while (window.isOpen())
+    {
+        //Handle player input
+        if (Keyboard::isKeyPressed(Keyboard::Escape))
+            window.close();
+
+        //Update the Scene
+
+        //Draw the Scene
+        window.clear();
+
+        window.display();
+    }
+
     return 0;
 }

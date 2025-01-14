cb5b8c205c747dd5525d40df32351c085a86dd38
Aaron <aaron@thelinkfamily.net>
2025-01-13 23:27:36 -0600

    Added HUD feature

--- Timber.cpp
+++ Timber.cpp
@@ -1,10 +1,13 @@
 // Timber.cpp : This file contains the 'main' function. Program execution begins and ends there.
 
 #include <SFML/Graphics.hpp>
+#include <sstream>
 #include "Bee.h"
 #include "Cloud.h"
 
 using namespace sf;
+
+void centerOrigin(Text&);
 
 const int NUM_BEES = 3;
 const int NUM_CLOUDS = 5;
@@ -17,6 +20,8 @@ int main()
     std::vector<Bee> bees((rand() % NUM_BEES) + 1);
     std::vector<Cloud> clouds((rand() % NUM_CLOUDS) + 3);
     bool paused = true;
+    std::stringstream ss;
+    int score = 0;
 
     //Create and open a window for the game
     RenderWindow window(VideoMode(1920, 1080), "Timber", Style::Fullscreen);
@@ -42,6 +47,36 @@ int main()
     //Load cloud texture on GPU memory
     Texture textureCloud;
     textureCloud.loadFromFile("graphics/cloud.png");
+
+    //Load font and create text variables
+    Font font;
+    font.loadFromFile("fonts/KOMIKAP_.ttf");
+
+    Text scoreText, messageText;
+    scoreText.setFont(font); //Setup score text
+    scoreText.setFillColor(Color::White);
+    scoreText.setCharacterSize(75);
+    ss << "Score: " << score;
+    scoreText.setString(ss.str());
+    scoreText.setPosition(20, 20);
+
+    messageText.setFont(font); //Setup message text
+    messageText.setFillColor(Color::White);
+    messageText.setCharacterSize(100);
+    messageText.setString("PRESS ENTER TO START!");
+    centerOrigin(messageText);
+    messageText.setPosition(1920 / 2.0f, 1080 / 2.0f);
+
+    RectangleShape timebar; //Setup timebar
+    float timebarWidth = 400;
+    float timebarHeight = 80;
+    timebar.setSize(Vector2f(timebarWidth, timebarHeight));
+    timebar.setFillColor(Color::Red);
+    timebar.setPosition(960 - timebarWidth / 2, 980);
+
+    Time gameTimeTotal;
+    float timeRemaining = 6.0f;
+    float timebarWidthPerSecond = timebarWidth / timeRemaining;
 
     //Init bees
     for (Bee& bee : bees)
@@ -105,6 +140,19 @@ int main()
                         cloud.setActive(false);
                 }
             }
+
+            //Update score
+            ss.str(std::string());
+            ss << "Score: " << score;
+            scoreText.setString(ss.str());
+
+            //Update timebar
+            timeRemaining -= dt.asSeconds();
+            timebar.setSize(Vector2f(timebarWidthPerSecond * timeRemaining, timebarHeight));
+            timebar.setPosition(960 - timebarWidthPerSecond * timeRemaining / 2, 980);
+
+            if (timeRemaining <= 0.0f)
+                paused = true;
         }
 
         //Draw the Scene
@@ -120,8 +168,21 @@ int main()
         for (Bee& bee : bees) //Draw bees
             window.draw(bee.getSprite());
 
+        window.draw(scoreText); //Draw score
+ 
+        window.draw(timebar); //Draw timebar
+
+        if (paused)
+            window.draw(messageText); //Draw message
+
         window.display();
 
     }
     return 0;
+}
+
+void centerOrigin(Text& text)
+{
+    FloatRect tempRect = text.getLocalBounds();
+    text.setOrigin(tempRect.left + tempRect.width / 2, tempRect.top + tempRect.height / 2);
 }

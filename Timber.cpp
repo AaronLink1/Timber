952f17047e161d08df91e9d07f30e9939fb15d2d
Aaron <aaron@thelinkfamily.net>
2025-01-15 14:45:03 -0600

    Added tree branches and functionality. Updated the player input to be handled by an event.

--- Timber.cpp
+++ Timber.cpp
@@ -7,15 +7,25 @@
 
 using namespace sf;
 
+//Function declarations
 void centerOrigin(Text&);
+void updateBranches();
+
+enum class side { LEFT = 0, RIGHT, NONE };
 
 const int NUM_BEES = 3;
 const int NUM_CLOUDS = 5;
+const int NUM_BRANCHES = 6;
+
+//Setup branches arrays
+Sprite branches[NUM_BRANCHES];
+side branchSides[NUM_BRANCHES];
 
 int main()
 {
     //Seed random num generator and declare variables
     srand((int)time(0));
+    Event event;
     Clock clock;
     std::vector<Bee> bees((rand() % NUM_BEES) + 1);
     std::vector<Cloud> clouds((rand() % NUM_CLOUDS) + 3);
@@ -39,6 +49,10 @@ int main()
     Sprite spriteTree;
     spriteTree.setTexture(textureTree);
     spriteTree.setPosition(810, 0);
+
+    //Load branch texture on GPU memory
+    Texture textureBranch;
+    textureBranch.loadFromFile("graphics/branch.png");
 
     //Load bee texture on GPU memory
     Texture textureBee;
@@ -78,6 +92,33 @@ int main()
     float timeRemaining = 6.0f;
     float timebarWidthPerSecond = timebarWidth / timeRemaining;
 
+    //Init tree branches
+    for (side& side : branchSides)
+    {
+        int random = rand() % 5;
+        switch (random)
+        {
+        case 0:
+            side = side::LEFT;
+            break;
+        case 1:
+            side = side::RIGHT;
+            break;
+        default:
+            side = side::NONE;
+            break;
+        }
+    }
+
+    for (int i = 0; i < NUM_BRANCHES; i++)
+    {
+        branches[i].setTexture(textureBranch);
+        branches[i].setPosition(-2000, -2000);
+        branches[i].setOrigin(220, 20);
+        if (branchSides[i] == side::LEFT)
+            branches[i].setRotation(180);
+    }
+
     //Init bees
     for (Bee& bee : bees)
     {
@@ -98,11 +139,33 @@ int main()
     while (window.isOpen())
     {
         //Handle player input
-        if (Keyboard::isKeyPressed(Keyboard::Escape)) //Close the game
-            window.close();
-
-        if (Keyboard::isKeyPressed(Keyboard::Return)) //Pause/unpause the game
-            paused = !paused;
+        while (window.pollEvent(event))
+        {
+            switch (event.type)
+            {
+            case Event::KeyPressed:
+                switch (event.key.code)
+                {
+                case Keyboard::Escape:
+                    window.close();
+                    break;
+                case Keyboard::Return:
+                    paused = !paused;
+                    break;
+                case Keyboard::Left:
+                    updateBranches();
+                    break;
+                case Keyboard::Right:
+                    updateBranches();
+                    break;
+                default:
+                    break;
+                }
+                break;
+            default:
+                break;
+            }
+        }
 
         //Update the Scene
         Time dt = clock.restart();
@@ -141,6 +204,18 @@ int main()
                 }
             }
 
+            for (int i = 0; i < NUM_BRANCHES; i++)
+            {
+                float height = i * 150;
+
+                if (branchSides[i] == side::LEFT)
+                    branches[i].setPosition(610, height);
+                else if (branchSides[i] == side::RIGHT)
+                    branches[i].setPosition(1330, height);
+                else
+                    branches[i].setPosition(3000, height);
+            }
+
             //Update score
             ss.str(std::string());
             ss << "Score: " << score;
@@ -162,6 +237,9 @@ int main()
 
         for (Cloud& cloud : clouds) //Draw clouds
             window.draw(cloud.getSprite());
+
+        for (Sprite branch : branches)
+            window.draw(branch);
 
         window.draw(spriteTree); //Draw tree
 
@@ -185,4 +263,29 @@ void centerOrigin(Text& text)
 {
     FloatRect tempRect = text.getLocalBounds();
     text.setOrigin(tempRect.left + tempRect.width / 2, tempRect.top + tempRect.height / 2);
+}
+
+void updateBranches()
+{
+    for (int i = NUM_BRANCHES - 1; i > 0; i--)
+        branches[i] = branches[i - 1];
+
+    for (int i = NUM_BRANCHES - 1; i > 0; i--)
+        branchSides[i] = branchSides[i - 1];
+
+    int random = rand() % 5;
+    switch (random)
+    {
+    case 0:
+        branchSides[0] = side::LEFT;
+        branches[0].setRotation(180);
+        break;
+    case 1:
+        branchSides[0] = side::RIGHT;
+        branches[0].setRotation(0);
+        break;
+    default:
+        branchSides[0] = side::NONE;
+        break;
+    }
 }
